15a66cfb99c04e1b2247d4d1c2c4cd64447b725e
laravelRo <71215083+laravelRo@users.noreply.github.com>
2021-10-20 17:52:25 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,7 @@ require __DIR__ . '/auth.php';
 require __DIR__ . '/front/pages.php';
 //rutele pentru panoul de control al utilizatorilor
 require __DIR__ . '/front/user.php';
-<<<<<<< Updated upstream
-=======
+// ==========
 
 //rutele pentru personalul administrativ al sitului
 require __DIR__ . '/admin/staff.php';
@@ -49,4 +48,3 @@ require __DIR__ . '/admin/content/sections.php';
 
 //rutele pentru sectiuni
 require __DIR__ . '/front/content/sections.php';
->>>>>>> Stashed changes
